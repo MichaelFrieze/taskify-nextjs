365d8b67cb126b046cb7a8ea195b32e080270d71
Michael Frieze <michaelfrieze@icloud.com>
2023-11-23 19:11:03 -0500

    Added title metadata to dashboard organization layout

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
+
 import { OrgControl } from "./_components/org-control";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
