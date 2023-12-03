3cf3cc24464cc02cdae050f63343831344de190e
Michael Frieze <michaelfrieze@icloud.com>
2023-12-02 23:39:37 -0500

    Installed hello-pangea drag and drop

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -9,7 +9,10 @@ interface CardItemProps {
 
 export const CardItem = ({ data, index }: CardItemProps) => {
   return (
-    <div className="truncate rounded-md border-2 border-transparent bg-white px-3 py-2 text-sm shadow-sm hover:border-black">
+    <div
+      role="button"
+      className="truncate rounded-md border-2 border-transparent bg-white px-3 py-2 text-sm shadow-sm hover:border-black"
+    >
       {data.title}
     </div>
   );
