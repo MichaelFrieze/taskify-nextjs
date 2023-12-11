4e03b0c810e08532a80f8953dcfd1561b41a6840
Michael Frieze <michaelfrieze@icloud.com>
2023-12-11 10:25:44 -0500

    fix: change some of the padding in logo and marketing page to make it look better

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -26,12 +26,12 @@ const MarketingPage = () => {
       >
         <div className="mb-4 flex items-center rounded-full border bg-amber-100 p-4 uppercase text-amber-700 shadow-sm">
           <Medal className="mr-2 h-6 w-6" />
-          No 1 task managment
+          <div className="pt-1">No 1 task managment</div>
         </div>
         <h1 className="mb-6 text-center text-3xl text-neutral-800 md:text-6xl">
           Taskify helps team move
         </h1>
-        <div className="w-fit rounded-md bg-gradient-to-r from-fuchsia-600 to-pink-600 p-2 px-4 pb-4 text-3xl text-white md:text-6xl">
+        <div className="w-fit rounded-md bg-gradient-to-r from-fuchsia-600 to-pink-600 p-2 px-4  text-3xl text-white md:text-6xl">
           work forward.
         </div>
       </div>
--- components/logo.tsx
+++ components/logo.tsx
@@ -13,11 +13,11 @@ export const Logo = () => {
     <Link href="/">
       <div className="hidden items-center gap-x-2 transition hover:opacity-75 md:flex">
         <Image src="/logo.svg" alt="Logo" height={30} width={30} />
-        <p
-          className={cn("pb-1 text-lg text-neutral-700", headingFont.className)}
+        <div
+          className={cn("pt-1 text-lg text-neutral-700", headingFont.className)}
         >
           Taskify
-        </p>
+        </div>
       </div>
     </Link>
   );
