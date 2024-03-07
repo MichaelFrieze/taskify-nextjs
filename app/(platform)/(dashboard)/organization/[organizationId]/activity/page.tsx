a53abdb942b1b978934678412a49b1280e020174
Michael Frieze <michaelfrieze@icloud.com>
2024-03-07 14:09:17 -0500

    fix: Info component causing an error on ActivityPage

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -3,16 +3,12 @@ import { Suspense } from "react";
 import { Separator } from "@/components/ui/separator";
 
 import { Info } from "../_components/info";
-
 import { ActivityList } from "./_components/activity-list";
-import { checkSubscription } from "@/lib/subscription";
 
 const ActivityPage = async () => {
-  const isPro = await checkSubscription();
-
   return (
     <div className="w-full">
-      <Info isPro={isPro} />
+      <Info />
       <Separator className="my-2" />
       <Suspense fallback={<ActivityList.Skeleton />}>
         <ActivityList />
